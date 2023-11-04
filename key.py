78d6e1dd3bf30a9fd2c580ec418524ff9dd63e6a
gavinwalters <gavin.j.walters@gmail.com>
2023-11-03 23:28:00 -0400

    fixed prompt input using data

--- key.py
+++ key.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 
-def generate_email(community):
+def generate_email(community, data):
     # Define the system message
     system_msg = f"""
     You are a knowlegable leads acquisition agent specializing in client prospecting and sales, working for TerraByte Solutions. TerraByte Solutions is a web development company that draws its inspiration and ethos from the pioneering spirit of exploration that characterized Newfoundland's history. It positions itself as a bridge between traditional pioneering values ("Terra Nova") and the digital frontier ("TerraByte"). The company aims to create robust and enduring websites, which not only serve as a business's digital presence but also as a platform for growth and engagement in the online ecosystem.
@@ -15,14 +15,14 @@ def generate_email(community):
 
     # Define the user message
     user_msg = f"""
-        Write a professional lead acquisition email for TerraByte Solutions. The email is addressed to the Municipal Manager of {community}. Mention our services in web design and development, cybersecurity, and AI-driven solutions. Highlight our commitment to enhancing economic development, quality of living, and population growth. Our aim is to partner with them to revitalize their community's digital presence. Begin with a greeting, introduce TerraByte Solutions, provide the offer, and end with a call to action.
+        Write a professional lead acquisition email for TerraByte Solutions. The email is addressed to the Municipal Manager of {community}. Mention our services in web design and development, cybersecurity, and AI-driven solutions. Highlight our commitment to enhancing economic development, quality of living, and population growth. Our aim is to partner with them to revitalize their community's digital presence. Begin with a greeting, introduce TerraByte Solutions, provide the offer drawing on specific data points provided in the data, and end with a call to action. Here is the data: {data}
         """
 
     # Create a dataset using GPT
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",
                                             messages=[{"role": "system", "content": system_msg},
                                             {"role": "user", "content": user_msg}])
-
+    return response
 
 
 #Function to get list of communities in directory
@@ -37,17 +37,21 @@ def get_filepaths(communities):
     filepaths= {}
     for community in communities:
         path = r'/Users/gavin/Documents/Townfolio data/' + community
-        filepaths[community].append(path)
+        filepaths[community]=path
     return filepaths
  
 # Function to read the .txt file and return structured data
 def read_dataset(file_path, community):
     # Implement the logic to read the file and structure the data
 
-    file = open(file_path, 'r')
-    content = 'Data for: ' + community + '\n' + file.read()
+    try:
+        file = open(file_path, 'r')
+        content = 'Data for: ' + community + '\n' + file.read()
 
-    return {community: content}
+        return content
+    except:
+        print(community + " not found at " + file_path)
+        
 
 
 # Main logic
@@ -57,13 +61,13 @@ def main():
     for key in filepaths:
         path = filepaths[key] + '/data.txt'
         communities_info = {}
-        communities_info. append(read_dataset(path, key))
+        communities_info[key]=read_dataset(path, key)
 
     # Generate and print emails for each community
     for community in communities_info:
-        email_content = generate_email(communities_info[community])
+        email_content = generate_email(community, communities_info[community])
         print(f"Email for {community}:\n")
-        print(email_content)
+        print(email_content['choices'][0]['message']['content'])
         print("-" * 40)
 
 if __name__ == "__main__":
