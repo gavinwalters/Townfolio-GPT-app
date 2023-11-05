6b20525e23d431dea38a0ec49dbcd553d9a96083
gavinwalters <gavin.j.walters@gmail.com>
2023-11-05 00:20:52 -0400

    added training & write email to file

--- key.py
+++ key.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import time
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -8,9 +9,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def generate_email(community, data):
+    training = open('trainingTxt.txt', 'r')
     # Define the system message
     system_msg = f"""
-    You are a knowlegable leads acquisition agent specializing in client prospecting and sales, working for TerraByte Solutions. TerraByte Solutions is a web development company that draws its inspiration and ethos from the pioneering spirit of exploration that characterized Newfoundland's history. It positions itself as a bridge between traditional pioneering values ("Terra Nova") and the digital frontier ("TerraByte"). The company aims to create robust and enduring websites, which not only serve as a business's digital presence but also as a platform for growth and engagement in the online ecosystem.
+    You are a knowlegable leads acquisition agent specializing in client prospecting and sales, working for TerraByte Solutions. TerraByte Solutions is a web development company that draws its inspiration and ethos from the pioneering spirit of exploration that characterized Newfoundland's history. It positions itself as a bridge between traditional pioneering values ("Terra Nova") and the digital frontier ("TerraByte"). The company aims to create robust and enduring websites, which not only serve as a business's digital presence but also as a platform for growth and engagement in the online ecosystem. {training}
     """
 
     # Define the user message
@@ -58,17 +60,32 @@ def read_dataset(file_path, community):
 def main():
     communities = read_communities()
     filepaths = get_filepaths(communities)
+    # print(filepaths)ç
+
+    communities_info = {}
     for key in filepaths:
         path = filepaths[key] + '/data.txt'
-        communities_info = {}
         communities_info[key]=read_dataset(path, key)
+        # print("key: " +key)
 
     # Generate and print emails for each community
     for community in communities_info:
+        # print(community)
         email_content = generate_email(community, communities_info[community])
         print(f"Email for {community}:\n")
         print(email_content['choices'][0]['message']['content'])
+        filename = str(community) + "-email.txt"
+        path = "/Users/gavin/Documents/Townfolio data/Emails/" + filename
+        try:
+            f = open(path, "x")
+            f.write(email_content['choices'][0]['message']['content'])
+            f.close()
+        except:
+            continue
+        # print(f.read())
         print("-" * 40)
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
